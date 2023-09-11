96ac46695e5f858f7ffc60fe3b5ff8fae9906c51
50372 <50372@MANUEL>
2023-09-10 20:34:39 -0600

    act

---
+++ Configuracion/Configuracion recomendada.Designer.cs
@@ -0,0 +1,381 @@
+﻿
+namespace Configuracion
+{
+    partial class Configuracion_recomendada
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Configuracion_recomendada));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.numerotxb = new System.Windows.Forms.TextBox();
+            this.numerorecomtxb = new System.Windows.Forms.TextBox();
+            this.separadorrecomtxb = new System.Windows.Forms.TextBox();
+            this.separadortxb = new System.Windows.Forms.TextBox();
+            this.sepdecrecomtxb = new System.Windows.Forms.TextBox();
+            this.sepdectxb = new System.Windows.Forms.TextBox();
+            this.simbolrecomtxb = new System.Windows.Forms.TextBox();
+            this.Simbolotxb = new System.Windows.Forms.TextBox();
+            this.simbolonegrecomtxb = new System.Windows.Forms.TextBox();
+            this.simbolonegtxb = new System.Windows.Forms.TextBox();
+            this.numerodecrecomtxb = new System.Windows.Forms.TextBox();
+            this.numerodectxb = new System.Windows.Forms.TextBox();
+            this.sepdecimrecomtxb = new System.Windows.Forms.TextBox();
+            this.sepdecimtxb = new System.Windows.Forms.TextBox();
+            this.sepmilesrecomtxb = new System.Windows.Forms.TextBox();
+            this.sepmilestxb = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnSalir = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Propiedad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(487, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Recomendado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(353, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Actual";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(86, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Numero de decimales(moneda)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(86, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(229, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Separador de decimales(moneda)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(86, 432);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(202, 22);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Separador de miles(numeros)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(86, 389);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(226, 22);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Separador de decimales(numero)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(86, 337);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(218, 22);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Numero de decimales(numeros)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(86, 291);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(119, 22);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Simbolo negativo";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(86, 242);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(139, 22);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Simbolo de moneda";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(86, 195);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(198, 22);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Separador de miles(moneda)";
+            // 
+            // numerotxb
+            // 
+            this.numerotxb.Location = new System.Drawing.Point(357, 107);
+            this.numerotxb.Name = "numerotxb";
+            this.numerotxb.Size = new System.Drawing.Size(110, 22);
+            this.numerotxb.TabIndex = 11;
+            // 
+            // numerorecomtxb
+            // 
+            this.numerorecomtxb.Location = new System.Drawing.Point(491, 107);
+            this.numerorecomtxb.Name = "numerorecomtxb";
+            this.numerorecomtxb.Size = new System.Drawing.Size(113, 22);
+            this.numerorecomtxb.TabIndex = 12;
+            // 
+            // separadorrecomtxb
+            // 
+            this.separadorrecomtxb.Location = new System.Drawing.Point(491, 153);
+            this.separadorrecomtxb.Name = "separadorrecomtxb";
+            this.separadorrecomtxb.Size = new System.Drawing.Size(113, 22);
+            this.separadorrecomtxb.TabIndex = 14;
+            // 
+            // separadortxb
+            // 
+            this.separadortxb.Location = new System.Drawing.Point(357, 153);
+            this.separadortxb.Name = "separadortxb";
+            this.separadortxb.Size = new System.Drawing.Size(110, 22);
+            this.separadortxb.TabIndex = 13;
+            // 
+            // sepdecrecomtxb
+            // 
+            this.sepdecrecomtxb.Location = new System.Drawing.Point(491, 196);
+            this.sepdecrecomtxb.Name = "sepdecrecomtxb";
+            this.sepdecrecomtxb.Size = new System.Drawing.Size(113, 22);
+            this.sepdecrecomtxb.TabIndex = 16;
+            // 
+            // sepdectxb
+            // 
+            this.sepdectxb.Location = new System.Drawing.Point(357, 196);
+            this.sepdectxb.Name = "sepdectxb";
+            this.sepdectxb.Size = new System.Drawing.Size(110, 22);
+            this.sepdectxb.TabIndex = 15;
+            // 
+            // simbolrecomtxb
+            // 
+            this.simbolrecomtxb.Location = new System.Drawing.Point(491, 243);
+            this.simbolrecomtxb.Name = "simbolrecomtxb";
+            this.simbolrecomtxb.Size = new System.Drawing.Size(113, 22);
+            this.simbolrecomtxb.TabIndex = 18;
+            // 
+            // Simbolotxb
+            // 
+            this.Simbolotxb.Location = new System.Drawing.Point(357, 243);
+            this.Simbolotxb.Name = "Simbolotxb";
+            this.Simbolotxb.Size = new System.Drawing.Size(110, 22);
+            this.Simbolotxb.TabIndex = 17;
+            // 
+            // simbolonegrecomtxb
+            // 
+            this.simbolonegrecomtxb.Location = new System.Drawing.Point(491, 292);
+            this.simbolonegrecomtxb.Name = "simbolonegrecomtxb";
+            this.simbolonegrecomtxb.Size = new System.Drawing.Size(113, 22);
+            this.simbolonegrecomtxb.TabIndex = 20;
+            // 
+            // simbolonegtxb
+            // 
+            this.simbolonegtxb.Location = new System.Drawing.Point(357, 292);
+            this.simbolonegtxb.Name = "simbolonegtxb";
+            this.simbolonegtxb.Size = new System.Drawing.Size(110, 22);
+            this.simbolonegtxb.TabIndex = 19;
+            // 
+            // numerodecrecomtxb
+            // 
+            this.numerodecrecomtxb.Location = new System.Drawing.Point(491, 338);
+            this.numerodecrecomtxb.Name = "numerodecrecomtxb";
+            this.numerodecrecomtxb.Size = new System.Drawing.Size(113, 22);
+            this.numerodecrecomtxb.TabIndex = 22;
+            // 
+            // numerodectxb
+            // 
+            this.numerodectxb.Location = new System.Drawing.Point(357, 338);
+            this.numerodectxb.Name = "numerodectxb";
+            this.numerodectxb.Size = new System.Drawing.Size(110, 22);
+            this.numerodectxb.TabIndex = 21;
+            // 
+            // sepdecimrecomtxb
+            // 
+            this.sepdecimrecomtxb.Location = new System.Drawing.Point(491, 390);
+            this.sepdecimrecomtxb.Name = "sepdecimrecomtxb";
+            this.sepdecimrecomtxb.Size = new System.Drawing.Size(113, 22);
+            this.sepdecimrecomtxb.TabIndex = 24;
+            // 
+            // sepdecimtxb
+            // 
+            this.sepdecimtxb.Location = new System.Drawing.Point(357, 390);
+            this.sepdecimtxb.Name = "sepdecimtxb";
+            this.sepdecimtxb.Size = new System.Drawing.Size(110, 22);
+            this.sepdecimtxb.TabIndex = 23;
+            // 
+            // sepmilesrecomtxb
+            // 
+            this.sepmilesrecomtxb.Location = new System.Drawing.Point(491, 433);
+            this.sepmilesrecomtxb.Name = "sepmilesrecomtxb";
+            this.sepmilesrecomtxb.Size = new System.Drawing.Size(113, 22);
+            this.sepmilesrecomtxb.TabIndex = 26;
+            // 
+            // sepmilestxb
+            // 
+            this.sepmilestxb.Location = new System.Drawing.Point(357, 433);
+            this.sepmilestxb.Name = "sepmilestxb";
+            this.sepmilestxb.Size = new System.Drawing.Size(110, 22);
+            this.sepmilestxb.TabIndex = 25;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnSalir});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(672, 57);
+            this.toolStrip1.TabIndex = 27;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Image = ((System.Drawing.Image)(resources.GetObject("btnSalir.Image")));
+            this.btnSalir.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnSalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(92, 54);
+            this.btnSalir.Text = "Salir";
+            // 
+            // Configuracion_recomendada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 485);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.sepmilesrecomtxb);
+            this.Controls.Add(this.sepmilestxb);
+            this.Controls.Add(this.sepdecimrecomtxb);
+            this.Controls.Add(this.sepdecimtxb);
+            this.Controls.Add(this.numerodecrecomtxb);
+            this.Controls.Add(this.numerodectxb);
+            this.Controls.Add(this.simbolonegrecomtxb);
+            this.Controls.Add(this.simbolonegtxb);
+            this.Controls.Add(this.simbolrecomtxb);
+            this.Controls.Add(this.Simbolotxb);
+            this.Controls.Add(this.sepdecrecomtxb);
+            this.Controls.Add(this.sepdectxb);
+            this.Controls.Add(this.separadorrecomtxb);
+            this.Controls.Add(this.separadortxb);
+            this.Controls.Add(this.numerorecomtxb);
+            this.Controls.Add(this.numerotxb);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Configuracion_recomendada";
+            this.Text = "Configuracion recomendada";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox numerotxb;
+        private System.Windows.Forms.TextBox numerorecomtxb;
+        private System.Windows.Forms.TextBox separadorrecomtxb;
+        private System.Windows.Forms.TextBox separadortxb;
+        private System.Windows.Forms.TextBox sepdecrecomtxb;
+        private System.Windows.Forms.TextBox sepdectxb;
+        private System.Windows.Forms.TextBox simbolrecomtxb;
+        private System.Windows.Forms.TextBox Simbolotxb;
+        private System.Windows.Forms.TextBox simbolonegrecomtxb;
+        private System.Windows.Forms.TextBox simbolonegtxb;
+        private System.Windows.Forms.TextBox numerodecrecomtxb;
+        private System.Windows.Forms.TextBox numerodectxb;
+        private System.Windows.Forms.TextBox sepdecimrecomtxb;
+        private System.Windows.Forms.TextBox sepdecimtxb;
+        private System.Windows.Forms.TextBox sepmilesrecomtxb;
+        private System.Windows.Forms.TextBox sepmilestxb;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnSalir;
+    }
+}
