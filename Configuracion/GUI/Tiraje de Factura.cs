d85cfcc73a2f20a42e1ee64b961ac0b0096cee5e
50372 <50372@MANUEL>
2023-09-10 19:17:54 -0600

    act

---
+++ Configuracion/GUI/Tiraje de Factura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Configuracion
+{
+    public partial class Tiraje_de_Factura : Form
+    {
+        public Tiraje_de_Factura()
+        {
+            InitializeComponent();
+        }
+    }
+}
